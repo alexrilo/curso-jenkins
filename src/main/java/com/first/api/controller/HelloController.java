d8157bdba378c03fabe56d37ab1b81ec3b71f680
alexrilo <alejandroriloferreiro@gmail.com>
2026-09-07 17:30:57 +0200

    Nuevos cambios webhook

--- src/main/java/com/first/api/controller/HelloController.java
+++ src/main/java/com/first/api/controller/HelloController.java
@@ -41,11 +41,16 @@ public class HelloController {
         return "Hola Mundo con REST en Spring Boot";
     }
 
-
     //Otro comentario
     @GetMapping("/hello6")
     public String saludar6(){
         return "Hola Mundo con REST en Spring Boot";
+    }
+
+    //Otro comentario
+    @GetMapping("/hello7")
+    public String saludar7(){
+        return "Hola Mundo con REST en Spring Boot 7";
     }
 }
 
